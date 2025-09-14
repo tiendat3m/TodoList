257795fe0fe1170258513edcadb0e84ec96db963
Tien Dat <phantiendat14012002@gmail.com>
2025-09-14 23:03:09 +0700

    ready to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,20 +3,30 @@ import taskRoute from './routes/tasksRouters.js'
 import { connecDb } from './config/db.js'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import path from 'path'
 dotenv.config()
 const port = process.env.PORT || '5001'
-
+const __dirname = path.resolve()
 const app = express()
 
 app.use(express.json())
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}))
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: 'http://localhost:5173',
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
+    }))
+}
 
 app.use('/api/tasks', taskRoute)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    })
+}
 
 connecDb().then(() => {
     app.listen(port, () => {
